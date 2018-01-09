e1d7d35bb60032c0f4c092ad76253693c4a04c55
Nolan Byrnes <nolanehs@gmail.com>
2018-01-09 18:39:56 +0000

    build room scraper

--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -1,6 +1,27 @@
 class RoomScraper
 
   def initialize(index_url)
-    @doc = Nokogiri::HTML(open(atl_scraper))
+    @doc = Nokogiri::HTML(open(index_url))
   end
+
+  def call
+    rows.each do |row.doc|
+      scrape_row(row_doc)
+    end
+  end
+
+  private
+  def rows
+    @rows ||= @doc.search("div.content span.rows p.row")
+  end
+
+  def scrape_row(row)
+{
+    time = row.search("time")
+    title = row.search("a.hdrlnk").text
+    url = row.search("a.hdrlnk").attribute("href").text
+    price = row.search("span.price").text
+}
+  end
+
 end
